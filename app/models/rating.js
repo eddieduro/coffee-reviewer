f620f2332492fc78cea1f420e21f30652ad2ae79
eddieduro <e.jamesduro@gmail.com>
2016-04-09 14:01:12 -0700

    ability to display coffee on index

--- app/models/rating.js
+++ app/models/rating.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   value: DS.attr('number'),
-  user: DS.belongsTo('user'. {async: true}),
+  user: DS.belongsTo('user', {async: true}),
   coffee: DS.belongsTo('coffee', {async: true})
 });
